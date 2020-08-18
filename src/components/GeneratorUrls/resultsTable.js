28e02d9a988715373f4ddc4ecea15ff9bd2cc69f
Denis Didenko <denis.didenko@mht.priv>
2020-08-18 11:21:42 +0300

    all previews

--- src/components/GeneratorUrls/resultsTable.js
+++ src/components/GeneratorUrls/resultsTable.js
@@ -2,37 +2,46 @@ import React from 'react';
 
 import { DOMAIN, ADMIN_PAGE, EDIT_PAGE, TESTING_PAGE, STATUS_PAGE, VIEW_PAGE, VIEW_PAGE_TESTING } from '../../config';
 
+function copy(data) {
+    const text = data.join('\n');
+    navigator.clipboard.writeText(text);
+}
+
 const ResultsTable = ({ list = [] }) => {
     if (!list || !list.length) return <p>No found matches</p>;
 
+    const allPreviews = [];
+
     function renderRows(rows) {
         return rows.map(({ lid, name }) => {
+            const adminLink = DOMAIN + ADMIN_PAGE + lid;
+            const editLink = DOMAIN + EDIT_PAGE + lid;
+            const previewLink = 'https://www.benaughty.com' + VIEW_PAGE + name + VIEW_PAGE_TESTING;
+            const testLink = DOMAIN + STATUS_PAGE + lid + TESTING_PAGE;
+
+            allPreviews.push(previewLink);
+
             return (
                 <tr key={lid}>
                     <td>{lid}</td>
                     <td>{name}</td>
                     <td>
-                        <a href={DOMAIN + ADMIN_PAGE + lid} className="btn" target="_blank" rel="noopener noreferrer">
+                        <a href={adminLink} className="btn" target="_blank" rel="noopener noreferrer">
                             admin
                         </a>
                     </td>
                     <td>
-                        <a href={DOMAIN + EDIT_PAGE + lid} className="btn" target="_blank" rel="noopener noreferrer">
+                        <a href={editLink} className="btn" target="_blank" rel="noopener noreferrer">
                             edit
                         </a>
                     </td>
                     <td>
-                        <a
-                            href={'https://www.benaughty.com' + VIEW_PAGE + name + VIEW_PAGE_TESTING}
-                            className="btn"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
+                        <a href={previewLink} className="btn" target="_blank" rel="noopener noreferrer">
                             preview
                         </a>
                     </td>
                     <td>
-                        <a href={DOMAIN + STATUS_PAGE + lid + TESTING_PAGE} className="btn" target="_blank" rel="noopener noreferrer">
+                        <a href={testLink} className="btn" target="_blank" rel="noopener noreferrer">
                             test
                         </a>
                     </td>
@@ -54,7 +63,21 @@ const ResultsTable = ({ list = [] }) => {
                 </tr>
             </thead>
 
-            <tbody>{renderRows(list)}</tbody>
+            <tbody>
+                {renderRows(list)}
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <button className="btn" onClick={copy(allPreviews)}>
+                            copy ALL previews
+                        </button>
+                    </td>
+                    <td></td>
+                </tr>
+            </tbody>
         </table>
     );
 };
