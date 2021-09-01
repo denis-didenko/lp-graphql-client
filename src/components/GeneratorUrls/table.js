c0b6a205e61d84d4bda841f8b2bc1fc13a6dbf48
Denis Didenko <denis.didenko@kv.sd.priv>
2021-09-01 10:45:13 +0300

    add new buttons

--- src/components/GeneratorUrls/table.js
+++ src/components/GeneratorUrls/table.js
@@ -1,97 +1,114 @@
-import React from 'react';
+import React from "react";
 
-import Loader from '../Loader';
-import useScreenshot from './useScreenshot';
-import { DOMAIN, ADMIN_PAGE, EDIT_PAGE, TESTING_PAGE, STATUS_PAGE, VIEW_PAGE, VIEW_PAGE_TESTING } from '../../config';
-import { copy } from '../../utils';
+import Loader from "../Loader";
+import useScreenshot from "./useScreenshot";
+import { DOMAIN, ADMIN_PAGE, EDIT_PAGE, TESTING_PAGE, STATUS_PAGE, VIEW_PAGE, VIEW_PAGE_TESTING } from "../../config";
+import { copy } from "../../utils";
 
 const ResultsTable = ({ list = [] }) => {
-    const { saveScreenshot, loading, error } = useScreenshot();
+	const { saveScreenshot, loading, error } = useScreenshot();
 
-    if (!list || !list.length) return <p>No found matches</p>;
+	if (!list || !list.length) return <p>No found matches</p>;
 
-    const allPreviews = [];
+	const allPreviews = [];
+	const allNames = [];
+	const allTestingLinks = [];
 
-    function renderRows(rows) {
-        return rows.map(({ lid, name }) => {
-            const adminLink = DOMAIN + ADMIN_PAGE + lid;
-            const editLink = DOMAIN + EDIT_PAGE + lid;
-            const previewLink = 'https://www.benaughty.com' + VIEW_PAGE + name;
-            const previewTestLink = 'https://www.benaughty.com' + VIEW_PAGE + name + VIEW_PAGE_TESTING;
-            const testLink = DOMAIN + STATUS_PAGE + lid + TESTING_PAGE;
+	const sendAllToTesting = (links) => {
+		links.forEach((link) => {
+			window.open(link, "_blank");
+		});
+	};
 
-            allPreviews.push(previewTestLink);
+	function renderRows(rows) {
+		return rows.map(({ lid, name }) => {
+			const adminLink = DOMAIN + ADMIN_PAGE + lid;
+			const editLink = DOMAIN + EDIT_PAGE + lid;
+			const previewTestLink = "https://m.youdates.com" + VIEW_PAGE + name + VIEW_PAGE_TESTING;
+			const testLink = DOMAIN + STATUS_PAGE + lid + TESTING_PAGE;
 
-            return (
-                <tr key={lid}>
-                    <td>{lid}</td>
-                    <td>{name}</td>
-                    <td>
-                        <a href={adminLink} className="btn" target="_blank" rel="noopener noreferrer">
-                            admin
-                        </a>
-                    </td>
-                    <td>
-                        <a href={editLink} className="btn" target="_blank" rel="noopener noreferrer">
-                            edit
-                        </a>
-                    </td>
-                    <td>
-                        <a href={previewTestLink} className="btn" target="_blank" rel="noopener noreferrer">
-                            preview
-                        </a>
-                    </td>
-                    <td>
-                        <a href={testLink} className="btn" target="_blank" rel="noopener noreferrer">
-                            test
-                        </a>
-                    </td>
-                    <td>
-                        {loading ? (
-                            <Loader />
-                        ) : (
-                            <button className="btn" onClick={e => saveScreenshot({ variables: { url: previewLink } })}>
-                                save screenshot
-                            </button>
-                        )}
-                    </td>
-                </tr>
-            );
-        });
-    }
+			allPreviews.push(previewTestLink);
+			allNames.push(name);
+			allTestingLinks.push(testLink);
 
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Admin</th>
-                    <th>Edit</th>
-                    <th>Preview</th>
-                    <th>Testing</th>
-                    <th>Screenshot</th>
-                </tr>
-            </thead>
+			return (
+				<tr key={lid}>
+					<td>{lid}</td>
+					<td>{name}</td>
+					<td>
+						<a href={adminLink} className="btn" target="_blank" rel="noopener noreferrer">
+							admin
+						</a>
+					</td>
+					<td>
+						<a href={editLink} className="btn" target="_blank" rel="noopener noreferrer">
+							edit
+						</a>
+					</td>
+					<td>
+						<a href={previewTestLink} className="btn" target="_blank" rel="noopener noreferrer">
+							preview
+						</a>
+					</td>
+					<td>
+						<a href={testLink} className="btn" target="_blank" rel="noopener noreferrer">
+							test
+						</a>
+					</td>
+					<td>
+						{loading ? (
+							<Loader />
+						) : (
+							<button className="btn" onClick={(e) => saveScreenshot({ variables: { url: previewTestLink } })}>
+								save screenshot
+							</button>
+						)}
+					</td>
+				</tr>
+			);
+		});
+	}
 
-            <tbody>
-                {renderRows(list)}
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>
-                        <button className="btn" onClick={() => copy(allPreviews)}>
-                            copy ALL previews
-                        </button>
-                    </td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
-    );
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Name</th>
+					<th>Admin</th>
+					<th>Edit</th>
+					<th>Preview</th>
+					<th>Testing</th>
+					<th>Screenshot</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				{renderRows(list)}
+				<tr>
+					<td></td>
+					<td>
+						<button className="btn" onClick={() => copy(allNames)}>
+							copy ALL names
+						</button>
+					</td>
+					<td></td>
+					<td></td>
+					<td>
+						<button className="btn" onClick={() => copy(allPreviews)}>
+							copy ALL previews
+						</button>
+					</td>
+					<td>
+						<button className="btn" onClick={() => sendAllToTesting(allTestingLinks)}>
+							send ALL to testing
+						</button>
+					</td>
+					<td></td>
+				</tr>
+			</tbody>
+		</table>
+	);
 };
 
 export default React.memo(ResultsTable);
